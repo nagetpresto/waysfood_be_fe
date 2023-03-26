86ba33c0e559263c8d2cc5ce4c430eba2a104562
unknown <bilqistaz27@gmail.com>
2023-03-26 10:52:20 +0700

    edited

--- server/main.go
+++ server/main.go
@@ -6,7 +6,7 @@ import (
 	"BE/routes"
 	"fmt"
 	"os"
-
+	
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -35,42 +35,5 @@ func main() {
 
 	var port = os.Getenv("PORT");
 	fmt.Println("server running localhost:" + port)
-	e.Logger.Fatal(e.Start(":" + port))
+	e.Logger.Fatal(e.Start("localhost:5000"))
 }
-// package main
-
-// import (
-// 	"BE/database"
-// 	"BE/pkg/mysql"
-// 	"BE/routes"
-// 	"fmt"
-	
-// 	"github.com/joho/godotenv"
-// 	"github.com/labstack/echo/v4"
-// 	"github.com/labstack/echo/v4/middleware"
-// )
-
-// func main() {
-// 	e := echo.New()
-
-// 	errEnv := godotenv.Load()
-// 	if errEnv != nil {
-// 		panic("Failed to load env file")
-// 	}
-
-// 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-// 		AllowOrigins: []string{"*"},
-// 		AllowMethods: []string{echo.GET, echo.POST, echo.PATCH, echo.DELETE},
-// 		AllowHeaders: []string{"X-Requested-With", "Content-Type", "Authorization"},
-// 	}))
-
-// 	e.Static("/uploads", "./uploads")
-
-// 	mysql.DatabaseInit()
-// 	database.RunMigration()
-
-// 	routes.RouteInit(e.Group("/api/v1"))
-
-// 	fmt.Println("server running localhost:5000")
-// 	e.Logger.Fatal(e.Start("localhost:5000"))
-// }
